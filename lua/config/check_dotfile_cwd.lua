7cf949ebae1ae5ad97f90f0d5f9d25ceac940355
Diego Velez <diego.velez.dev@gmail.com>
2025-09-30 18:07:02 -0400

    ui: Add notification for .files environment

--- lua/config/check_dotfile_cwd.lua
+++ lua/config/check_dotfile_cwd.lua
@@ -15,6 +15,7 @@ local dotfiles_files = vim
 local cwd = vim.fn.getcwd()
 cwd = string.sub(cwd, #home + 2)
 if cwd ~= nil and string.find(dotfiles_files.stdout, cwd, 1, true) ~= nil then
+  vim.notify('Using .files environment', vim.log.levels.INFO)
   vim.env.GIT_WORK_TREE = home
   vim.env.GIT_DIR = git_dir
 end
@@ -25,9 +26,11 @@ vim.api.nvim_create_user_command(
   function(_)
     local config_on = vim.env.GIT_WORK_TREE ~= nil and vim.env.GIT_DIR ~= nil
     if config_on then
+      vim.notify('Disabled .files environment', vim.log.levels.INFO)
       vim.env.GIT_WORK_TREE = nil
       vim.env.GIT_DIR = nil
     else
+      vim.notify('Enabled .files environment', vim.log.levels.INFO)
       vim.env.GIT_WORK_TREE = home
       vim.env.GIT_DIR = git_dir
     end
