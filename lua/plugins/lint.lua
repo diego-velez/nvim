19eec38ac0cd1ef0191ff4ff9811a8f1c56a35dc
Diego Velez <diego.velez.dev@gmail.com>
2025-10-23 12:13:22 -0400

    fix: Linter toggle would not stay disabled/hidden

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -31,32 +31,26 @@ lint.linters_by_ft['ruby'] = nil
 lint.linters_by_ft['terraform'] = nil
 lint.linters_by_ft['text'] = nil
 
--- Create autocommand which carries out the actual linting
--- on the specified events.
--- Allows toggling the linter on or off
-vim.g.lint_enabled = true
-local toggle_lint = function()
-  vim.g.lint_enabled = not vim.g.lint_enabled
-  if vim.g.lint_enabled then
-    vim.diagnostic.show(nil, 0)
-    vim.notify('Lint enabled', vim.log.levels.INFO)
-  else
-    vim.diagnostic.hide(nil, 0)
-    vim.notify('Lint disabled', vim.log.levels.INFO)
-  end
-end
-
-local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+-- Create autocommand which carries out the actual linting on the specified events.
 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-  group = lint_augroup,
+  desc = 'Run linting for current modifiable buffer',
+  group = vim.api.nvim_create_augroup('lint', { clear = true }),
   callback = function()
-    -- Only run the linter when it is enabled, and in buffers that you
+    -- Only run the linter in buffers that you
     -- can modify in order to avoid superfluous noise, notably within
     -- the handy LSP pop-ups that describe the hovered symbol using Markdown.
-    if vim.g.lint_enabled and vim.bo.modifiable then
+    if vim.bo.modifiable then
       lint.try_lint()
     end
   end,
 })
 
-vim.keymap.set('n', '<leader>tl', toggle_lint, { desc = 'Toggle [L]inter' })
+vim.keymap.set('n', '<leader>tl', function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+
+  if vim.diagnostic.is_enabled() then
+    vim.notify('Lint enabled', vim.log.levels.INFO)
+  else
+    vim.notify('Lint disabled', vim.log.levels.INFO)
+  end
+end, { desc = 'Toggle [L]inter' })
