bd79e09305414d15cfd45fda7c1285bec16ccfc4
Diego Velez <diego.velez.dev@gmail.com>
2026-04-04 18:39:42 -0400

    feat: only set dotfiles git env vars if not in git repo

--- lua/config/check_dotfile_cwd.lua
+++ lua/config/check_dotfile_cwd.lua
@@ -2,22 +2,30 @@
 local home = vim.fn.getenv 'HOME'
 local git_dir = home .. '/.files'
 local work_tree = home
-local dotfiles_files = vim
-  .system({
-    'git',
-    '--git-dir=' .. git_dir,
-    '--work-tree=' .. work_tree,
-    'ls-files',
-    '--full-name',
-    home,
-  }, { text = true })
+
+-- If we are in a standard repo (like a submodule) we do not want to set as if in dotfiles
+local is_in_standard_repo = vim
+  .system({ 'git', 'rev-parse', '--is-inside-work-tree' }, { text = true })
   :wait()
-local cwd = vim.fn.getcwd()
-cwd = string.sub(cwd, #home + 2)
-if cwd ~= nil and string.find(dotfiles_files.stdout, cwd, 1, true) ~= nil then
-  vim.notify('Using .files environment', vim.log.levels.INFO)
-  vim.env.GIT_WORK_TREE = home
-  vim.env.GIT_DIR = git_dir
+
+if is_in_standard_repo.code ~= 0 then
+  local dotfiles_files = vim
+    .system({
+      'git',
+      '--git-dir=' .. git_dir,
+      '--work-tree=' .. work_tree,
+      'ls-files',
+      '--full-name',
+      home,
+    }, { text = true })
+    :wait()
+  local cwd = vim.fn.getcwd()
+  cwd = string.sub(cwd, #home + 2)
+  if cwd ~= nil and string.find(dotfiles_files.stdout, cwd, 1, true) ~= nil then
+    vim.notify('Using .files environment', vim.log.levels.INFO)
+    vim.env.GIT_WORK_TREE = home
+    vim.env.GIT_DIR = git_dir
+  end
 end
 
 vim.api.nvim_create_user_command(
